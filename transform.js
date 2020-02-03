8ae97663164cd40b24dcdffd850916e3b7c1ffc2
김수헌 <20152766@sungshin.ac.kr>
2020-02-03 11:58:01 +0900

    transform

---
+++ transform.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-item').onclick = function(){
+    document.querySelector('#container').classList.remove('page1');
+    document.querySelector('#container').classList.add('page2');
+
+}
+
+document.querySelector('.back').onclick = function(){
+    document.querySelector('#container').classList.remove('page2');
+    document.querySelector('#container').classList.add('page1');
+    
+}
+
